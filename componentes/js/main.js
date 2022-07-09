9cd6b89720d81ad1b77b6866f4ae1799878a2d09
WillyRLV <williamlv1998@gmail.com>
2022-07-09 15:25:38 -0500

    sabado09

--- componentes/js/main.js
+++ componentes/js/main.js
@@ -1,46 +1,66 @@
 /*MOSTRAR MENU*/
-const navMenu = document.getElementById('nav-menu'),
-navToggle = document.getElementById('nav-toggle'),
-navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
-
-
-if (navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
-
-};
-
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
 //? MENU HIDDEN
 
-if (navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
-
-};
-
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 //? REMOVE MENU MOBILE
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
+  const navMenu = document.getElementById("nav-menu");
 
-    navMenu.classList.remove('show-menu')
+  navMenu.classList.remove("show-menu");
 };
 
-navLink.forEach(n => n.addEventListener('click', linkAction));
-
-
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*CHANGE BACKGOUND HEADER]============================*/
 
 function scrollHeader() {
-    const header = document.getElementById('header')
-    if(this.scrollY >= 80) header.classList.add('scroll-header');
-    else header.classList.remove('scroll-header')
-};
+  const header = document.getElementById("header");
+  if (this.scrollY >= 80) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
 
-window.addEventListener('scroll',scrollHeader);
+window.addEventListener("scroll", scrollHeader);
+
+/*acordion de preguntas============================*/
+
+const accordionItems = document.querySelectorAll(".questions__item");
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".questions__header");
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.questions__content')
+    if (item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }
+    else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
